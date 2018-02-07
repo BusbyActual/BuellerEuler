813b74ac402b0b67e95096f383c1e9b565ea084e
Ryan Busby <BusbyActual@gmail.com>
2018-02-06 19:33:20 -0800

    half solved prompt

--- src/4.palindrome.js
+++ src/4.palindrome.js
@@ -5,12 +5,27 @@
 
   Find the largest palindrome made from the product of two 3-digit numbers.
 */
+// let check
+let findPalindrome = (count, digits) => {
+  let highest = 0;
+  let dict = {};
+  let max  = '';
+  let counter = digits;
 
-let palindrome = ( ) => {
+  while (counter > 1) {
+    max += 9
+    temp--;
+  }
 
+  for (let x = counter; x > 1; x-- ) {
+    for (let y = counter; y > 1; y--) {
+      let temp = x * y;
+
+    }
+  }
 
 };
 
-const answer = palindrome( );
+const answer = findPalindrome(2, 3);
 
 console.log(answer);
