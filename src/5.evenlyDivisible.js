c50453fa87d46647009c78c10f33138300a7dc22
Ryan Busby <BusbyActual@gmail.com>
2018-02-06 20:01:08 -0800

    solved prompt

--- src/5.evenlyDivisible.js
+++ src/5.evenlyDivisible.js
@@ -6,10 +6,29 @@
   What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 */
 
-let evenlyDivisible = ( ) => {
+let isDivisible = (num, max) => {
+  let result = true;
 
+  for (let x = max; x > 0; x--) {
+    if (num % x !== 0) {
+      result = false;
+      x = 0;
+    }
+  }
+
+  return result;
 };
 
-const answer = evenlyDivisible( );
+let evenlyDivisible = (num) => {
+  let count = num;
+
+  while(!isDivisible(count, num)) {
+    count++;
+  }
+
+  return count;
+};
+
+const answer = evenlyDivisible(20);
 
 console.log(answer);
