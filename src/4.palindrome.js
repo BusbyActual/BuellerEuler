2b706c1fe02db32481082875c0a62adc6d107491
Ryan Busby <BusbyActual@gmail.com>
2018-02-06 19:39:12 -0800

    Added func to check if palindrome

--- src/4.palindrome.js
+++ src/4.palindrome.js
@@ -5,7 +5,20 @@
 
   Find the largest palindrome made from the product of two 3-digit numbers.
 */
-// let check
+
+let checkPalindrome = (num) => {
+  let myString = '' + num;
+
+  /* reverse removeChar for comparison*/
+  let isPalindrome = removeChar.split('').reverse().join('');
+
+  if (myString === isPalindrome) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
 let findPalindrome = (count, digits) => {
   let highest = 0;
   let dict = {};
@@ -20,6 +33,7 @@ let findPalindrome = (count, digits) => {
   for (let x = counter; x > 1; x-- ) {
     for (let y = counter; y > 1; y--) {
       let temp = x * y;
+
 
     }
   }
