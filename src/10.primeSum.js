80216ee31707ae2e45a177d6c0def839edc89c64
Ryan Busby <BusbyActual@gmail.com>
2018-02-08 21:39:53 -0800

    Solved /flex

--- src/10.primeSum.js
+++ src/10.primeSum.js
@@ -6,10 +6,24 @@
   Find the sum of all the primes below two million.
 */
 
-let primeSum = ( ) => {
+let primeHelper = require('./7.getPrimes.js');
+/*
+   let me just require an old prompt to break the rules ; )
+*/
+let primeSum = (max) => {
+  let primes = [];
+  let total = 0;
 
+  for (let x = 0; x < max; x++) {
+    if (primeHelper.isPrime(x)) {
+      primes.push(x);
+      total += x;
+    }
+  }
+
+  return { 'total': total, 'primes': primes };
 };
 
-const answer = primeSum( );
+const answer = primeSum(2000000);
 
 console.log(answer);
