9856e5f612460948a649ee466c805fc9dbed5c45
Ryan Busby <BusbyActual@gmail.com>
2018-02-21 22:30:08 -0800

    Solved prompt : ^)

--- src/15.latticePaths.js
+++ src/15.latticePaths.js
@@ -6,10 +6,26 @@
   How many such routes are there through a 20×20 grid?
 */
 
-let lettuce = ( ) => {
+let lettuce = (grid) => {
+  let count = 0;
 
+  let subroutine = (x, y) => {
+     if (x < grid) {
+      subroutine(x + 1, y);
+     }
+    if (y < grid) {
+      subroutine(x, y + 1);
+    }
+    if (y === grid && x === grid) {
+      count++;
+    }
+  };
+
+  subroutine(0, 0);
+
+  return count;
 };
 
-const answer = lettuce( );
+const answer = lettuce(20);
 
 console.log(answer);
