d6badf950a10a62fa0ac68399f3d7a7ca48b1837
Ryan Busby <BusbyActual@gmail.com>
2018-02-21 22:39:04 -0800

    progress

--- src/16.powerSum.js
+++ src/16.powerSum.js
@@ -6,10 +6,15 @@
   What is the sum of the digits of the number 21000?
 */
 
-let power = ( ) => {
+let power = (base, power) => {
 
 };
 
-const answer = power( );
+let sum = (num) => {
+  let temp = '' + num;
+  return temp.split('').reduce((a,b) => { return (a * 1) + (b * 1); }, 0);
+};
+
+const answer = power(2,15);
 
 console.log(answer);
