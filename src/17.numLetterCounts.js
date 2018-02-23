c59dc3ac4afefcde6862424f6fb486b4e835771f
Ryan Busby <BusbyActual@gmail.com>
2018-02-22 20:35:08 -0800

    solved prompt

--- src/17.numLetterCounts.js
+++ src/17.numLetterCounts.js
@@ -12,11 +12,12 @@
 let letterCount = (max) => {
   let total = 0;
   let dict = {
+    0: '',
     1: 'one',
     2: 'two',
     3: 'three',
     4: 'four',
-    5: 'fix',
+    5: 'five',
     6: 'six',
     7: 'seven',
     8: 'eight',
@@ -44,29 +45,53 @@ let letterCount = (max) => {
   };
 
   let findHundreds = (num) => {
+    let count = 0;
+
     if (num !== 1000 && num > 99) {
-      return num % 100;
-    } else {
-      return 0;
+      while (num >= 100) {
+        num -= 100;
+        count++;
+      }
     }
+
+    return count;
   };
 
-  for (let x = 1; x < max; x++) {
+  for (let x = 1; x <= max; x++) {
     let h = findHundreds(x);
 
     if (x === 1000) {
       total += dict[1].length + dict[1000].length;
-    } else if (h) {
-      total += dict[h].length + dict[100].length;
-    } else {
+    } else if (x > 99) {
+      let str = '' + x;
+      let doubleDigits = str.slice(1);
+      let singleDigit = doubleDigits.slice(1);
+      doubleDigits = doubleDigits.slice(1) + '0';
 
+      if (dict[doubleDigits]) {
+        total += dict[doubleDigits].length;
+      }
+      total += dict[h].length + dict[100].length + dict[singleDigit].length;
+    } else {
+      let str = '' + x;
+      let doubleDigits;
+      let singleDigit;
+
+      if (str.length > 1 && !dict[x]) {
+        doubleDigits = str;
+        singleDigit = doubleDigits.slice(1);
+        doubleDigits = doubleDigits.slice(1) + '0';
+
+        total += dict[doubleDigits].length + dict[singleDigit].length;
+      } else {
+        total += dict[x].length;
+      }
     }
   }
-
 
   return total;
 };
 
-const answer = letterCount(5);
+const answer = letterCount(342);
 
 console.log(answer);
