0cd07490b71414020fe1d5bee58dbc8ce5264f28
Ryan Busby <BusbyActual@gmail.com>
2018-02-24 14:11:27 -0800

    progress

--- src/18.maxPaths.js
+++ src/18.maxPaths.js
@@ -1,91 +1,64 @@
 /*
   ### Problem
 
+  By starting at the top of the triangle below and moving to adjacent numbers on the row below, the maximum total from top to bottom is 23.
+
+  3
+  7 4
+  2 4 6
+  8 5 9 3
+
+  That is, 3 + 7 + 4 + 9 = 23.
+
+  Find the maximum total from top to bottom of the triangle below:
+
+  75
+  95 64
+  17 47 82
+  18 35 87 10
+  20 04 82 47 65
+  19 01 23 75 03 34
+  88 02 77 73 07 63 67
+  99 65 04 28 06 16 70 92
+  41 41 26 56 83 40 80 70 33
+  41 48 72 33 47 32 37 16 94 29
+  53 71 44 65 25 43 91 52 97 51 14
+  70 11 33 28 77 73 17 78 39 68 17 57
+  91 71 52 38 17 14 91 43 58 50 27 29 48
+  63 66 04 68 89 53 67 30 73 16 69 87 40 31
+  04 62 98 27 23 09 70 98 73 93 38 53 60 04 23
+
+  NOTE: As there are only 16384 routes, it is possible to solve this problem by trying every route. However, Problem 67, is the same challenge with a triangle containing one-hundred rows; it cannot be solved by brute force, and requires a clever method! ;o)
 */
 
-let letterCount = (max) => {
-  let total = 0;
-  let dict = {
-    0: '',
-    1: 'one',
-    2: 'two',
-    3: 'three',
-    4: 'four',
-    5: 'five',
-    6: 'six',
-    7: 'seven',
-    8: 'eight',
-    9: 'nine',
-    10: 'ten',
-    11: 'eleven',
-    12: 'twelve',
-    13: 'thirteen',
-    14: 'fourteen',
-    15: 'fifteen',
-    16: 'sixteen',
-    17: 'seventeen',
-    18: 'eighteen',
-    19: 'nineteen',
-    20: 'twenty',
-    30: 'thirty',
-    40: 'fourty',
-    50: 'fifty',
-    60: 'sixty',
-    70: 'seventy',
-    80: 'eighty',
-    90: 'ninety',
-    100: 'hundred',
-    1000: 'thousand'
-  };
+let triangle =
+[
+  [75],
+  [95, 64],
+  [17, 47, 82],
+  [18, 35, 87, 10],
+  [20, 04, 82, 47, 65],
+  [19, 01, 23, 75, 03, 34],
+  [88, 02, 77, 73, 07, 63, 67],
+  [99, 65, 04, 28, 06, 16, 70, 92],
+  [41, 41, 26, 56, 83, 40, 80, 70, 33],
+  [41, 48, 72, 33, 47, 32, 37, 16, 94, 29],
+  [53, 71, 44, 65, 25, 43, 91, 52, 97, 51, 14],
+  [70, 11, 33, 28, 77, 73, 17, 78, 39, 68, 17, 57],
+  [91, 71, 52, 38, 17, 14, 91, 43, 58, 50, 27, 29, 48],
+  [63, 66, 04, 68, 89, 53, 67, 30, 73, 16, 69, 87, 40, 31],
+  [04, 62, 98, 27, 23, 09, 70, 98, 73, 93, 38, 53, 60, 04, 23]
 
-  let findHundreds = (num) => {
-    let count = 0;
+];
 
-    if (num !== 1000 && num > 99) {
-      while (num >= 100) {
-        num -= 100;
-        count++;
-      }
-    }
+let maxPaths = (arr) => {
+  let triangle = arr[0];
+  let total;
+  let solution = [];
 
-    return count;
-  };
-
-  for (let x = 1; x <= max; x++) {
-    let h = findHundreds(x);
-
-    if (x === 1000) {
-      total += dict[1].length + dict[1000].length;
-    } else if (x > 99) {
-      let str = '' + x;
-      let doubleDigits = str.slice(1);
-      let singleDigit = doubleDigits.slice(1);
-      doubleDigits = doubleDigits.slice(1) + '0';
-
-      if (dict[doubleDigits]) {
-        total += dict[doubleDigits].length;
-      }
-      total += dict[h].length + dict[100].length + dict[singleDigit].length;
-    } else {
-      let str = '' + x;
-      let doubleDigits;
-      let singleDigit;
-
-      if (str.length > 1 && !dict[x]) {
-        doubleDigits = str;
-        singleDigit = doubleDigits.slice(1);
-        doubleDigits = doubleDigits.slice(1) + '0';
-
-        total += dict[doubleDigits].length + dict[singleDigit].length;
-      } else {
-        total += dict[x].length;
-      }
-    }
-  }
-
-  return total;
+  return { 'answer': total, 'solution': solution };
 };
 
-const answer = letterCount(342);
+const answer = maxPathss(triangle);
 
 console.log(answer);
